a89bc9c7a0d51078d16b337a4d6fcfe74d8cb0f4
mmilijic <mihajlo@zucchabar.com>
2020-03-10 13:32:35 +0100

    contex menu component reverted

--- addon/components/context-menu.js
+++ addon/components/context-menu.js
@@ -3,74 +3,51 @@ import invokeAction from 'ember-invoke-action';
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { htmlSafe } from '@ember/string';
-import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
+import { computed, get } from '@ember/object';
 
-export default class EmberContextMenu extends Component.extend(layout) {
+export default Component.extend({
+  layout,
 
-  @service('context-menu') contextMenu;
+  contextMenu: service('context-menu'),
 
-  @computed('contextMenu.isActive') get isActive() {
-    return this.contextMenu.isActive;
-  }
+  isActive:   reads('contextMenu.isActive'),
+  renderLeft: reads('contextMenu.renderLeft'),
+  items:      reads('contextMenu.items'),
+  _selection: reads('contextMenu.selection'),
+  details:    reads('contextMenu.details'),
+  clickEvent: reads('contextMenu.event'),
 
-  @computed('contextMenu.renderLeft') get renderLeft() {
-    return this.contextMenu.renderLeft;
-  }
-
-  @computed('contextMenu.items') get items() {
-    return this.contextMenu.items;
-  }
-
-  @computed('contextMenu.selection') get _selection() {
-    return this.contextMenu.selection;
-  }
-
-  @computed('contextMenu.details') get details() {
-    return this.contextMenu.details;
-  }
-
-  @computed('contextMenu.event') get clickEvent() {
-    return this.contextMenu.event;
-  }
-
-  @computed('_selection.[]')
-  get selection() {
-    return [].concat(this._selection);
-  }
-
-  @computed('contextMenu')
-  get cmClass() {
-    return this.contextMenu.ContextMenuClass;
-  }
+  selection: computed('_selection.[]', function() {
+    return [].concat(get(this, '_selection'));
+  }),
 
   didInsertElement() {
-    super.didInsertElement(...arguments);
+    this._super(...arguments);
     this.setWormholeTarget();
-  }
+  },
 
   setWormholeTarget() {
     let id = 'wormhole-context-menu';
-    let target = document.querySelectorAll(`#${ id }`);
+    let target = document.querySelectorAll(`#${id}`);
     if (target.length === 0) {
-      document.body.insertAdjacentHTML('beforeend', `<div id="${ id }"></div>`);
+      document.body.insertAdjacentHTML('beforeend', `<div id="${id}"></div>`);
     }
-  }
+  },
 
-  @computed('contextMenu.position.{left,top}')
-  get position() {
-    let { left, top } = this.contextMenu.position || {};
-    return htmlSafe(`left: ${ left }px; top: ${ top }px;`);
-  }
+  position: computed('contextMenu.position.{left,top}', function() {
+    let { left, top } = get(this, 'contextMenu.position') || {};
+    return htmlSafe(`left: ${left}px; top: ${top}px;`);
+  }),
 
-  @computed('selection.[]', 'details')
-  get itemIsDisabled() {
-    let selection = this.selection || [];
-    let details = this.details;
+  itemIsDisabled: computed('selection.[]', 'details', function() {
+    let selection = get(this, 'selection') || [];
+    let details   = get(this, 'details');
 
-    return function (item) {
-      let disabled = this.item.disabled;
+    return function(item) {
+      let disabled  = get(item, 'disabled');
 
-      if (!this.item.action && !this.item.subActions) {
+      if (!get(item, 'action') && !get(item, 'subActions')) {
         return true;
       }
 
@@ -80,16 +57,15 @@ export default class EmberContextMenu extends Component.extend(layout) {
 
       return disabled;
     };
-  }
+  }),
 
-  @computed('selection.[]', 'details')
-  get clickAction() {
-    let selection = this.selection;
-    let details = this.details;
-    let event = this.clickEvent;
+  clickAction: computed('selection.[]', 'details', function() {
+    let selection = get(this, 'selection');
+    let details   = get(this, 'details');
+    let event     = get(this, 'clickEvent');
 
-    return function (item) {
+    return function(item) {
       invokeAction(item, 'action', selection, details, event);
     };
-  }
-}
+  })
+});
